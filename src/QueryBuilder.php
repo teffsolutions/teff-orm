d25e6442d3d2909218bddca20085ccaf1c315ed9
Robin Wang Mac <robin@teffsolutions.com>
2021-10-21 12:02:23 -0400

    Update QueryBuilder.php

--- src/QueryBuilder.php
+++ src/QueryBuilder.php
@@ -1,5 +1,5 @@
 <?php 
-namespace SwimmingPool;
+namespace TeffOrm;
 
 class QueryBuilder{
 
@@ -8,6 +8,7 @@ class QueryBuilder{
   protected $base_table;
 
   protected $where;
+  protected $left_join;
 
   public function __construct($base_table){
     $this->base_table=$base_table;
@@ -21,14 +22,15 @@ class QueryBuilder{
 
     return $this;
   }
-  public function hasMany($target_table, $foreign_key, $key_value){
-    global $db;
-    $query="SELECT ".implode(',', $select)." FROM ".$this->target_table;
-    $query.=" WHERE $foreign_key=$key_value";
 
+  public function join(){
 
-    echo $query;
   }
+
+  public function query(){
+
+  }
+  
   public function where($condition){
     $this->where.=' '.$condition.' ';
     return $this;
@@ -43,12 +45,81 @@ class QueryBuilder{
     $query.=$this->query;
 
     if (!empty($this->where)) $query.=$this->where;
-    
 
     echo $query;
     // $query.='where '.$this->where;
     // $rs = sql_prep($query, $db, array());
 
+
+  }
+
+  public function hasMany($target_class, $foreign_key, $key_value){
+    global $db;
+
+    $target_table = (new $target_class)->getTable();
+
+    $query="SELECT * FROM ".$target_table;
+    $query.=" WHERE $foreign_key=$key_value";
+
+    // echo $query;
+    $_arr = array();
+    $rs = sql_prep($query, $db, array($_id));
+    while ($__row=sql_fetch_assoc($rs)) {
+      $__obj = new $target_class();
+      
+      $__obj->setAttributes($__row);
+      $_arr[]=$__obj;
+    } // while
+    return $_arr;
+  }
+
+  public function belongsToMany($local_primary_value, $target_class,$pivot_table, $pivot_local, $pivot_foreign){
+    global $db;
+    $target_table = (new $target_class)->getTable();
+
+    $query="SELECT $target_table.* FROM $target_table";
+    $query.=" JOIN $pivot_table ON $target_table.$pivot_foreign = $pivot_table.$pivot_foreign";
+    $query.=" WHERE $pivot_table.$pivot_local=$local_primary_value";
+
+    $_arr=array();
+
+    $rs = sql_prep($query, $db, array());
+
+    while ($__row=sql_fetch_assoc($rs)) {
+      $__obj = new $target_class();
+    
+      $__obj->setAttributes($__row);
+      $_arr[]=$__obj;
+    } // while
+    return $_arr;
+  }
+
+  public function belongsTo($target_class, $foreign_key, $key_value){
+
+    global $db;
+
+    $target_table = (new $target_class)->getTable();
+    
+    $query="SELECT * FROM ".$target_table;
+    $query.=" WHERE $foreign_key=$key_value";
+
+    $_arr = array();
+    $rs = sql_prep($query, $db, array($_id));
+    while ($__row=sql_fetch_assoc($rs)) {
+      $__obj = new $target_class();
+      
+      $__obj->setAttributes($__row);
+      $_arr[]=$__obj;
+    } // while
+    return $_arr;
+
+  }
+
+  public function hasOne(){
+
+  }
+
+  public function with(){
 
   }
 
